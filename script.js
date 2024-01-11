b24a92a88229ce831803cb8fbe160042b3b9540b
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-01-11 22:20:11 +0500

    Homework is start!

---
+++ script.js
@@ -0,0 +1,29 @@
+// try {
+//   alert("salom")
+//   console.log("hello");
+//   console.log(test);
+//   alert("bye")
+// } catch (error) {
+// console.log("xatolik", error);  
+// }
+
+// setTimeout(() => {
+//   console.log("Hello world");
+//   console.log("Bye");
+// }, 2000);
+
+// setInterval(() => {
+//   console.log("Hello Javascript");
+// }, 1000);
+// try {
+//   console.log("Hello");
+//   console.log(test);
+// } catch (err) {
+//   // console.log(err.name);
+//   // console.log(err.message);
+//   console.log(err.stack);
+// }
+
+import {sum} from "./functions.js";
+
+console.log(sum(10, 5));
