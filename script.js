6bb396884ab58c08476e5c9609c8748cf1958f5d
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-01-12 07:49:44 +0500

    Homework is doone!

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
-// try {
-//   alert("salom")
-//   console.log("hello");
-//   console.log(test);
-//   alert("bye")
-// } catch (error) {
-// console.log("xatolik", error);  
+// <===========================> 1-masala <===============================>
+// let arr = [5, "salom", 2, true, 1, false, "Xayr"];
+
+// let sum = 0;
+// function number() {
+
+//   for (let iterator of arr) {
+//     if (typeof iterator === 'number')
+//     {
+//       sum += iterator;
+//     }
+//   }
+//   return sum;
 // }
+// let result = number(arr);
+// console.log("Jami " + result);
 
-// setTimeout(() => {
-//   console.log("Hello world");
-//   console.log("Bye");
-// }, 2000);
+// <===========================> 2-masala <===============================>
 
-// setInterval(() => {
-//   console.log("Hello Javascript");
-// }, 1000);
-// try {
-//   console.log("Hello");
-//   console.log(test);
-// } catch (err) {
-//   // console.log(err.name);
-//   // console.log(err.message);
-//   console.log(err.stack);
+let kichikSozlar = ["bolalar", "tarona", "andy", `name`];
+
+function upperCase(words) {
+  let kattalar = [];
+
+  for (let first of words) {
+
+    if (first.length > 0) {
+      let boshHarf = first[0].toUpperCase();
+      let kichikharf = first.slice(1);
+
+      let newWords = boshHarf + kichikharf;
+      kattalar.push(newWords);
+    } 
+
+  }
+  return kattalar;
+}
+
+let yangiSozlar = upperCase(kichikSozlar);
+console.log("Bosh harfli so'zlar: " + yangiSozlar);
+
+// <===========================> 3-masala <===============================>
+
+// let soz = "arra"; //prompt("Biror so'z kiriting");
+
+// function isPalind() {
+//   let teskariSoz = Array.from(soz).reverse().join("");
+
+//   if (soz == teskariSoz) {
+//     return "Bu palindrom so'z ekan!";
+//   } else {
+//     return "Bu palindrom so'z emas!";
+//   }
 // }
-
-import {sum} from "./functions.js";
-
-console.log(sum(10, 5));
+// console.log(isPalind(soz));
